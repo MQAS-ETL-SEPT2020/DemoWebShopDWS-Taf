1c65d3c516108b6ee0eb8cf26af8794e84e34753
Preeti <Preeti@DESKTOP-85FK620>
2022-11-11 22:19:55 +0530

    Updated Automation Script on ComputerExecution.java and ExcelData.xlsx

--- DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/ComputerExecution.java
+++ DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/ComputerExecution.java
@@ -1,5 +1,6 @@
 package com.DemoWebShopapp.tests;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 import java.util.Map;
 import org.automation.base.BaseTest;
@@ -20,12 +21,16 @@ public class ComputerExecution extends BaseTest {
 //		login.password.enterText(Configuration.get("password"));
 //		login.loginButton.click(LoginPageDWS.class);
 
+		// LOGIN PAGE
 		HomePageDWS home = new HomePageDWS();
 		LoginPageDWS login = home.login.click(LoginPageDWS.class);
 		login.email.enterText(data.get("Username"));
+		assertEquals(login.email.getAttributeValue("value"), data.get("Assertion1"),
+				"Email is not entered into Email textfield");
 		login.password.enterText(data.get("Password"));
 		login = login.loginButton.click(LoginPageDWS.class);
 
+		// DESKTOPS PAGE
 		ComputerPage computer = home.computers.click(ComputerPage.class);
 		assertTrue(computer.computertext.isVisible(), "computer page is not displayed");
 		computer.desktops.click();
@@ -33,49 +38,84 @@ public class ComputerExecution extends BaseTest {
 		assertTrue(computer.DisplayText.isVisible(), "By default Display per page dropdown is not selected as 8 ");
 		assertTrue(computer.ViewAsText.isVisible(), "By default ViewAs dropdown is not selected as Grid ");
 		computer.computer.click();
+
+		// DESKTOPS SUB PAGE
 		assertTrue(computer.ExpensivecomputerText.isVisible(), "By default Expensivecomputer page  is not displayed");
 		assertTrue(computer.Processor.isVisible(), "By default Processor is not selected as  Medium [+15.00]");
 		assertTrue(computer.RAM.isVisible(), "By default Ram is not selected as 2GB");
 		computer.Softwares.click();
 		assertTrue(computer.Quality.isVisible(), "By default Quality will be 1");
 		computer.AddtoCart.click();
-//		assertTrue(computer.message.isVisible(),"The product is added to cart message is not displayed");
 		computer.shoppingcart.click();
+
+		// SHOPPING CART PAGE
 		assertTrue(computer.shoppingcartText.isVisible(), "shopping cart page is not displayed");
 		computer.Terms.check();
 		computer.checkout.click();
 
-//	@Test(dataProvider = "ExcelDataProvider")
-//	public void checkout(Map<String, String> data) throws InterruptedException {
+		// CHECKOUT PAGE
 		CheckoutPage checkout1 = new CheckoutPage();
 		assertTrue(checkout1.CheckoutPage.isVisible(), "Checkoutpage is not displayed");
-		Thread.sleep(2000);
-		;
+		
+		// BILLING ADDRESS
 		checkout1.BillingAddressDropDown.selectByVisibleText(data.get("Dropdown Visible Text"));
+		assertEquals(checkout1.BillingAddressDropDown.getSelectedOption(), data.get("Assertion3"),
+				"Incorrect option selected from dropdown");
+
 		checkout1.FirstName.enterText(data.get("FirstName"));
+		assertEquals(checkout1.FirstName.getAttributeValue("value"), data.get("Assertion4"),
+				"FirstName is not entered into FirstName textfield");
+
 		checkout1.LastName.enterText(data.get("LastName"));
+		assertEquals(checkout1.LastName.getAttributeValue("value"), data.get("Assertion5"),
+				"LastName is not entered into LastName textfield");
+
 		checkout1.Email.enterText(data.get("Email"));
-		checkout1.CountryIdInBilling.selectByVisibleText(data.get("Dropdown Value"));
-		checkout1.City.enterText("City");
+		assertEquals(checkout1.Email.getAttributeValue("value"), data.get("Assertion6"),
+				"Email is not entered into Email textfield");
+
+		checkout1.CountryIdInBilling.selectByVisibleText(data.get("Country Dropdown Text"));
+		assertEquals(checkout1.CountryIdInBilling.getSelectedOption(), data.get("Assertion7"),
+				"Incorrect option selected from dropdown");
+
+		checkout1.City.enterText(data.get("City"));
+		assertEquals(checkout1.City.getAttributeValue("value"), data.get("Assertion8"), "Incorrect City");
+
 		checkout1.Address1InBilling.enterText(data.get("Address1"));
+		assertEquals(checkout1.Address1InBilling.getAttributeValue("value"), data.get("Assertion9"),
+				"Incorrect Address");
+
 		checkout1.ZipPostalCodeInBilling.enterText(data.get("Zip/Postal Code"));
+		assertEquals(checkout1.ZipPostalCodeInBilling.getAttributeValue("value"), data.get("Assertion10"),
+				"Incorrect Zip/Postal Code");
+
 		checkout1.PhoneNumberInBilling.enterText(data.get("Phone Number"));
+		assertEquals(checkout1.PhoneNumberInBilling.getAttributeValue("value"), data.get("Assertion11"),
+				"Incorrect phone number");
 		checkout1.ContinueInBilling.click();
+
+		// SHIPPING ADDRESS
+		assertTrue(checkout1.ShippingAddressFeild.isVisible(), "Shipping Address Feild is not displayed");
 		checkout1.InStorePickup.check();
-//    Thread.sleep(2000);
+		assertTrue(checkout1.InStorePickup.isChecked(), "InStorePickup  Checkbox is not checked");
 		checkout1.ContinueinShipping.click();
 
+		// PAYMENT METHOD
 		assertTrue(checkout1.PaymentMethodField.isVisible(), "PaymentMethodField page is not displayed");
 		checkout1.CashOnDelivery.click();
 		checkout1.ContinueInPaymentMethod.click();
 
+		// PAYMENT INFORMATION
 		assertTrue(checkout1.PaymentInformationField.isVisible(), "Payment information page is displayed");
 		assertTrue(checkout1.CodText.isVisible(), "You will pay by cash on delivery message is not displayed");
 		checkout1.ContinueInPaymentInformation.click();
 
+		// CONFIRMATION ORDER
+		assertTrue(checkout1.ConfirmOrderField.isVisible(), "Confirm Order Feild is not displayed");
+		assertTrue(checkout1.BillingAddressText.isVisible(), "Billing  Address Text  is not displayed");
 		checkout1.ConfirmInConfirmationOrder.click();
-		assertTrue(checkout1.ConfirmationMessage.isVisible(), "confirmation message is not displayed");
 
+		// THANKYOU PAGE
 		assertTrue(checkout1.ThankYouPage.isVisible(), "Thank you page is not displayed");
 		assertTrue(checkout1.OrderNumber.isVisible(), "Order number 1315200 is not displayed");
 		checkout1.ContinueInThankYouPage.click();

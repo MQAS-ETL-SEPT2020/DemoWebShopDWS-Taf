60c1ac909f12a9bbd1e93d7178085a6c1795b7bd
vanishree0321 <110149568+vanishree0321@users.noreply.github.com>
2022-11-15 12:38:10 +0530

    Updated
    Config.properties
    JewelryExecution
    JewelryPage
    Pom.xml
    
    Deleted
    README

--- DemoWebShopDWS_Taf/src/main/java/com/DemoWebShopapp/pages/JewelryPage.java
+++ DemoWebShopDWS_Taf/src/main/java/com/DemoWebShopapp/pages/JewelryPage.java
@@ -1,63 +1,46 @@
-//package com.DemoWebShopapp.pages;
-	package com.DemoWebShopapp.pages;
+package com.DemoWebShopapp.pages;
 
-	//package com.DemoWebShopapp.pages;
+import org.automation.base.BasePage;
+import org.automation.elements.Button;
+import org.automation.elements.CheckBox;
+import org.automation.elements.DropDown;
+import org.automation.elements.Element;
+import org.automation.elements.HyperLink;
+import org.automation.elements.TextBox;
+import org.openqa.selenium.By;
 
-		//import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+/**
+ * 
+ * @author TYS-Vanishree
+ * @version 1.0.0
+ * @since 14/11/2022
+ *
+ */
+public class JewelryPage extends BasePage {
+	public final Element jewelText = new Element("JewelpageText", By.xpath("//h1[text()='Jewelry']"));
+	public final DropDown option = new DropDown("Sort by dropdown", By.id("products-orderby"));
+	public final DropDown sortby = new DropDown("Position", By.xpath("//option[text()='Position']"));
+	public final DropDown option1 = new DropDown("Display dropdown", By.id("products-pagesize"));
+	public final DropDown display = new DropDown("8", By.xpath("//option[text()='8']"));
+	public final DropDown option2 = new DropDown("View as dropdown", By.id("products-viewmode"));
+	public final DropDown viewas = new DropDown("8", By.xpath("//option[text()='Grid']"));
+	public final HyperLink link = new HyperLink("Black&whitediamondheart",
+			By.xpath("(//h2[@class='product-title'])[2]"));
+	public final Element BlackandWhiteDiamond = new Element("black&whitediamondheartpage",
+			By.xpath("//strong[text()='Black & White Diamond Heart']"));
+	public final TextBox qty = new TextBox("Qty", By.id("addtocart_14_EnteredQuantity"));
+	public final Element cartpop = new Element("popupmsg",
+			By.xpath("//p[text()='The product has been added to your ']"));
+	public final Element shoppingpage = new Element("ShoppingPage", By.xpath("//h1[text()='Shopping cart']"));
+	public final Button add = new Button("Addtocart", By.id("add-to-cart-button-14"));
+	public final HyperLink shoplink = new HyperLink("shoppingcartlink", By.xpath("//span[text()='Shopping cart']"));
+	public final DropDown country = new DropDown("Display dropdown", By.id("CountryId"));
+	public final DropDown Selectedcountry = new DropDown("SelectIndia", By.xpath("//option[text()='India']"));
+	public final DropDown State = new DropDown("StateProvince", By.id("StateProvinceId"));
+	public final DropDown SelectedState = new DropDown("SelectState", By.id("StateProvinceId"));
+	public final TextBox postalcode = new TextBox("Zipcode", By.id("ZipPostalCode"));
+	public final Button estimate = new Button("Estimate", By.name("estimateshipping"));
+	public final CheckBox Terms = new CheckBox("Terms&conditions", By.id("termsofservice"));
+	public final Button checkout = new Button("CheckOut", By.id("checkout"));
 
-		//import org.apache.poi.sl.usermodel.TextBox;
-		import org.automation.base.BasePage;
-		import org.automation.elements.Button;
-		import org.automation.elements.CheckBox;
-		import org.automation.elements.DropDown;
-		import org.automation.elements.Element;
-		import org.automation.elements.HyperLink;
-		import org.automation.elements.TextBox;
-		//import org.automation.logger.Log;
-		import org.openqa.selenium.By;
-		//import org.openqa.selenium.WebDriver;
-		//import org.openqa.selenium.WebElement;
-		//import org.openqa.selenium.chrome.ChromeDriver;
-		//import org.openqa.selenium.interactions.Actions;
-		//import org.openqa.selenium.support.ui.Select;
-		//import org.openqa.selenium.support.ui.Wait;
-		/**
-		 * 
-		 * @author TYS-Vanishree
-		 * @version 1.0.0
-		 * @since 14/11/2022
-		 *
-		 */
-		public class JewelryPage extends BasePage
-		{
-			public final Element jewelText=new Element("JewelpageText",By.xpath("//h1[text()='Jewelry']"));
-			public final DropDown option = new DropDown("Sort by dropdown", By.id("products-orderby"));
-			public final DropDown sortby=new DropDown("Position",By.xpath("//option[text()='Position']"));
-			public final DropDown option1 = new DropDown("Display dropdown", By.id("products-pagesize"));
-			public final DropDown display=new DropDown("8",By.xpath("//option[text()='8']"));
-			public final DropDown option2 = new DropDown("View as dropdown", By.id("products-viewmode"));
-			public final DropDown viewas=new DropDown("8",By.xpath("//option[text()='Grid']"));
-			public final HyperLink link = new HyperLink("Black&whitediamondheart", By.xpath("(//h2[@class='product-title'])[2]"));
-			public final Element BlackandWhiteDiamond=new Element("black&whitediamondheartpage",By.xpath("//strong[text()='Black & White Diamond Heart']"));
-			public final TextBox qty=new TextBox("Qty", By.id("addtocart_14_EnteredQuantity"));
-		    public final Element cartpop=new Element("popupmsg",By.xpath("//p[text()='The product has been added to your ']"));
-		    public final Element shoppingpage=new Element("ShoppingPage",By.xpath("//h1[text()='Shopping cart']"));
-			public final Button add=new Button("Addtocart",By.id("add-to-cart-button-14"));
-		    public final HyperLink shoplink=new HyperLink("shoppingcartlink",By.xpath("//span[text()='Shopping cart']"));
-		    public final DropDown country = new DropDown("Display dropdown", By.id("CountryId"));
-		    public final DropDown Selectedcountry = new DropDown("SelectIndia", By.xpath("//option[text()='India']"));
-		    public final DropDown State=new DropDown("StateProvince",By.id("StateProvinceId"));
-		    public final DropDown SelectedState=new DropDown("SelectState",By.id("StateProvinceId"));
-		    public final TextBox postalcode=new TextBox("Zipcode", By.id("ZipPostalCode"));
-		    public final Button estimate=new Button("Estimate",By.name("estimateshipping"));
-		    public final CheckBox Terms=new CheckBox("Terms&conditions",By.id("termsofservice"));
-		    //public final CheckBox termsandcond=new CheckBox("Termscondtions",By.id("termsofservice"));
-		    public final Button checkout=new Button("CheckOut",By.id("checkout"));
-		    //public final Element CheckoutText=new Element("Checkoutpage",By.xpath("//h1[text()='Checkout']"));
-		    
-		}
-
-
-
-
-
+}
--- DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/JewelryExecution.java
+++ DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/JewelryExecution.java
@@ -1,178 +1,150 @@
-//package com.DemoWebShopapp.tests;
-	package com.DemoWebShopapp.tests;
-	import static org.testng.Assert.assertEquals;
-	import static org.testng.Assert.assertTrue;
-	//import static org.testng.Assert.assertTrue;
-	import java.awt.AWTException;
-	import java.awt.Robot;
-	import java.awt.event.KeyEvent;
-	import java.util.Map;
-	//import java.util.Map;
-	//import org.automation.base.BasePage;
-	import org.automation.base.BaseTest;
-	import org.automation.config.DriverType;
-	import org.automation.elements.Alert;
-	import org.automation.logger.Log;
-	import org.automation.utilities.Configuration;
-	import org.openqa.selenium.JavascriptExecutor;
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.chrome.ChromeDriver;
-	import org.openqa.selenium.interactions.Action;
-	import org.openqa.selenium.interactions.Actions;
-	import org.openqa.selenium.support.ui.Select;
-	import org.openqa.selenium.support.ui.WebDriverWait;
-	//import org.automation.utilities.Configuration;
-	import org.testng.annotations.Test;
-	import com.DemoWebShopapp.pages.CheckoutPage;
-	import com.DemoWebShopapp.pages.HomePageDWS;
-	import com.DemoWebShopapp.pages.JewelryPage;
-	import com.DemoWebShopapp.pages.LoginPageDWS;
+package com.DemoWebShopapp.tests;
 
-	/**
-	 * 
-	 * @author TYS-Vanishree
-	 * @version 1.0.0
-	 * @since 14/11/2022
-	 *
-	 */
-	public class JewelryExecution extends BaseTest {
-//			@Test
-		// public void Jewelrypagetest() throws InterruptedException, AWTException {
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Map;
+import org.automation.base.BaseTest;
+import org.automation.logger.Log;
+import org.testng.annotations.Test;
+import com.DemoWebShopapp.pages.CheckoutPage;
+import com.DemoWebShopapp.pages.HomePageDWS;
+import com.DemoWebShopapp.pages.JewelryPage;
+import com.DemoWebShopapp.pages.LoginPageDWS;
 
-//				
-//				HomePageDWS home=new HomePageDWS();
-//				LoginPageDWS login = home.login.click(LoginPageDWS.class);
-//				login.email.enterText(Configuration.get("username"));
-//				login.password.enterText(Configuration.get("password"));
-//				login.loginButton.click(LoginPageDWS.class);
-		@Test(dataProvider = "ExcelDataProvider")
-		public void checkout(Map<String, String> data) throws InterruptedException, AWTException {
-			
-			// Home Page
-			HomePageDWS home = new HomePageDWS();
-			
-			// login Page
-			LoginPageDWS login = home.login.click(LoginPageDWS.class);
-            assertTrue(login.email.isVisible(), "Not  clicking on Login link");
-			assertTrue(login.password.isVisible(), "Not clicking on Login Link");
-			assertTrue(login.loginButton.isVisible(), "Not clicking on Login Link");
-			login.email.enterText(data.get("Username"));
-			Log.info("Entering  the Email into the Email Textfield");
-			assertEquals(login.email.getAttributeValue("value"), data.get("Username_Assertion"),
-					"Email is not entered into the Email");
-			login.password.enterText(data.get("Password"));
-			Log.info("Entering the Email into the Password Textfield");
-			assertEquals(login.password.getAttributeValue("value"), data.get("Password_Assertion"),
-					"Password is not entered into the Email");
-            login = login.loginButton.click(LoginPageDWS.class);
+/**
+ * 
+ * @author TYS-Vanishree
+ * @version 1.0.0
+ * @since 14/11/2022
+ *
+ */
+public class JewelryExecution extends BaseTest {
 
-			HomePageDWS homepage = new HomePageDWS();
-			assertTrue(homepage.books.isVisible(), "Not clicking on Login Button");
-			assertTrue(homepage.computers.isVisible(), "Not clicking on Login Button");
-			assertTrue(homepage.electronics.isVisible(), "Not clicking on Login Button");
-			assertTrue(homepage.apparelAndShoes.isVisible(), "Not clicking on login button");
-			assertTrue(homepage.digitalDownloads.isVisible(), "Not clicking on Login Button");
-			assertTrue(homepage.jewellery.isVisible(), "Not clicking on login button");
-			assertTrue(homepage.giftCards.isVisible(), "Not clicking on Login Button");
-             
-			//JewelryPage
-			JewelryPage jewel = home.jewellery.click(JewelryPage.class);
-			assertTrue(jewel.jewelText.isVisible(), "Jewelry Page is not displayed");
-			assertTrue(jewel.sortby.isVisible(), "By default position is not displayed");
-			assertTrue(jewel.display.isVisible(), "By default 8 is not displayed");
-			assertTrue(jewel.viewas.isVisible(), "By default Grid is not displayed");
-			jewel.link.click();
+	@Test(dataProvider = "ExcelDataProvider")
+	public void checkout(Map<String, String> data) throws InterruptedException, AWTException {
 
-			assertTrue(jewel.BlackandWhiteDiamond.isVisible(), "product page is not displayed");
-			jewel.add.click();
+		// Home Page
+		HomePageDWS home = new HomePageDWS();
 
-			assertTrue(jewel.cartpop.isVisible(), "Addtocart popup msg is not displaying");
-			Robot rbt = new Robot();
-			rbt.keyPress(KeyEvent.VK_UP);
-			rbt.keyRelease(KeyEvent.VK_UP);
-			Thread.sleep(2000);
-			Thread.sleep(2000);
-			jewel.shoplink.click();
-			assertTrue(jewel.shoppingpage.isVisible(), "Shoppingpage is not displaying");
+		// login Page
+		LoginPageDWS login = home.login.click(LoginPageDWS.class);
+		assertTrue(login.email.isVisible(), "Not  clicking on Login link");
+		assertTrue(login.password.isVisible(), "Not clicking on Login Link");
+		assertTrue(login.loginButton.isVisible(), "Not clicking on Login Link");
+		login.email.enterText(data.get("Username"));
+		Log.info("Entering  the Email into the Email Textfield");
+		assertEquals(login.email.getAttributeValue("value"), data.get("Username_Assertion"),
+				"Email is not entered into the Email");
+		login.password.enterText(data.get("Password"));
+		Log.info("Entering the Email into the Password Textfield");
+		assertEquals(login.password.getAttributeValue("value"), data.get("Password_Assertion"),
+				"Password is not entered into the Email");
+		login = login.loginButton.click(LoginPageDWS.class);
 
-			jewel.Terms.check();
-			// assertTrue(jewel.termsandcond.isChecked(), "Checkbox is not Checked");
-			jewel.checkout.click();
-             
-			//CheckoutPage
-			CheckoutPage checkout1 = new CheckoutPage();
-			 assertTrue(checkout1.CheckoutPage.isVisible(), "Checkoutpage is not displayed");
-			 
-			//BillingAddressPage  
-			checkout1.BillingAddressDropDown.selectByVisibleText(data.get("Dropdown Visible Text"));
-			// assertTrue(checkout1.);
-			assertEquals(checkout1.BillingAddressDropDown.getSelectedOption(), data.get("Assertion1"),
-					"Incorrect option selected from dropdown");
-			
-			checkout1.FirstName.enterText(data.get("FirstName"));
-			Log.info("Entering  the FirstName into the FirstName Textfield");
-			assertEquals(checkout1.FirstName.getAttributeValue("value"), data.get("Assertion2"),
-					"FirstName is not entered into the FirstName Textfield");
-			checkout1.LastName.enterText(data.get("LastName"));
-			Log.info("Entering  the LastName into the LastName Textfield");
-			assertEquals(checkout1.LastName.getAttributeValue("value"), data.get("Assertion3"),
-					"LastName is not entered into the LastName Textfield");
-			checkout1.Email.enterText(data.get("Email"));
-			Log.info("Entering  the Email into the Email Textfield");
-			assertEquals(checkout1.Email.getAttributeValue("value"), data.get("Assertion4"),
-					"Email is not entered into the LastName Textfield");
-			checkout1.CountryIdInBilling.selectByVisibleText(data.get("Country Dropdown Text"));
-			assertEquals(checkout1.CountryIdInBilling.getSelectedOption(), data.get("Assertion5"),
-					"Incorrect option selected from dropdown");
-			checkout1.City.enterText(data.get("City"));
-			assertEquals(checkout1.City.getAttributeValue("value"), data.get("Assertion6"), "Incorrect City");
-			checkout1.Address1InBilling.enterText(data.get("Address1"));
-			assertEquals(checkout1.Address1InBilling.getAttributeValue("value"), data.get("Assertion7"),
-					"Incorrect Address1");
-			checkout1.ZipPostalCodeInBilling.enterText(data.get("Zip/Postal Code"));
-			assertEquals(checkout1.ZipPostalCodeInBilling.getAttributeValue("value"), data.get("Assertion8"),
-					"Incorrect Zip/Postal Code");
-			checkout1.PhoneNumberInBilling.enterText(data.get("Phone Number"));
-			assertEquals(checkout1.PhoneNumberInBilling.getAttributeValue("value"), data.get("Assertion9"),
-					"Incorrect Phone Number");
-			assertTrue(checkout1.ContinueInBilling.isEnabled(), "Continue In Billing is  disabled");
-			checkout1.ContinueInBilling.click();
-            
-			//ShippingAddressPage
-			assertTrue(checkout1.ShippingAddressFeild.isVisible(), "Shipping Address Feild is not displayed");
-			checkout1.InStorePickup.check();
-			assertTrue(checkout1.InStorePickup.isChecked(), "InStorePickup  Checkbox is not checked");
-			checkout1.ContinueinShipping.click();
+		HomePageDWS homepage = new HomePageDWS();
+		assertTrue(homepage.books.isVisible(), "Not clicking on Login Button");
+		assertTrue(homepage.computers.isVisible(), "Not clicking on Login Button");
+		assertTrue(homepage.electronics.isVisible(), "Not clicking on Login Button");
+		assertTrue(homepage.apparelAndShoes.isVisible(), "Not clicking on login button");
+		assertTrue(homepage.digitalDownloads.isVisible(), "Not clicking on Login Button");
+		assertTrue(homepage.jewellery.isVisible(), "Not clicking on login button");
+		assertTrue(homepage.giftCards.isVisible(), "Not clicking on Login Button");
 
-			// Payment Method
-			assertTrue(checkout1.PaymentMethodField.isVisible(), "Payment Method Feild is not displayed");
-			checkout1.CheckMoneyOrder.click();
-			assertTrue(checkout1.ContinueInPaymentMethod.isEnabled(), "Continue In Payment Method is  disabled");
-			checkout1.ContinueInPaymentMethod.click();
+		// JewelryPage
+		JewelryPage jewel = home.jewellery.click(JewelryPage.class);
+		assertTrue(jewel.jewelText.isVisible(), "Jewelry Page is not displayed");
+		assertTrue(jewel.sortby.isVisible(), "By default position is not displayed");
+		assertTrue(jewel.display.isVisible(), "By default 8 is not displayed");
+		assertTrue(jewel.viewas.isVisible(), "By default Grid is not displayed");
+		jewel.link.click();
 
-			// Payment Information
-			assertTrue(checkout1.PaymentInformationField.isVisible(), "Payment Information Feild is not displayed");
-			assertTrue(checkout1.ContinueInPaymentInformation.isEnabled(), "Continue In Payment Information  is  disabled");
-			checkout1.ContinueInPaymentInformation.click();
+		assertTrue(jewel.BlackandWhiteDiamond.isVisible(), "product page is not displayed");
+		jewel.add.click();
 
-			// Confirmation Order
-			assertTrue(checkout1.ConfirmOrderField.isVisible(), "Confirm Order Feild is not displayed");
-			assertTrue(checkout1.BillingAddressText.isVisible(), "Billing  Address Text  is not displayed");
-			assertTrue(checkout1.ConfirmInConfirmationOrder.isEnabled(), "Confirm In ConfirmationOrder is  diasbled");
-			checkout1.ConfirmInConfirmationOrder.click();
+		assertTrue(jewel.cartpop.isVisible(), "Addtocart popup msg is not displaying");
+		Robot rbt = new Robot();
+		rbt.keyPress(KeyEvent.VK_UP);
+		rbt.keyRelease(KeyEvent.VK_UP);
+		Thread.sleep(2000);
+		Thread.sleep(2000);
+		jewel.shoplink.click();
+		assertTrue(jewel.shoppingpage.isVisible(), "Shoppingpage is not displaying");
 
-			// ThankYou Page
-			assertTrue(checkout1.ThankYouPage.isVisible(), "ThankYouPage is not displayed");
-			assertTrue(checkout1.OrderNumber.isVisible(), "OrderNumber is not displayed");
-			assertTrue(checkout1.ConfirmationMessage.isVisible(), "ConfirmationMessage is not displayed");
-			assertTrue(checkout1.ContinueInThankYouPage.isEnabled(), "Continue In ThankYouPage is  disabled");
-			checkout1.ContinueInThankYouPage.click();
-			assertTrue(checkout1.ApplicationHomePage.isVisible(), "Application HomePage is displayed");
+		jewel.Terms.check();
+		jewel.checkout.click();
 
-		}
+		// CheckoutPage
+		CheckoutPage checkout1 = new CheckoutPage();
+		assertTrue(checkout1.CheckoutPage.isVisible(), "Checkoutpage is not displayed");
+
+		// BillingAddressPage
+		checkout1.BillingAddressDropDown.selectByVisibleText(data.get("Dropdown Visible Text"));
+		assertEquals(checkout1.BillingAddressDropDown.getSelectedOption(), data.get("Assertion1"),
+				"Incorrect option selected from dropdown");
+
+		checkout1.FirstName.enterText(data.get("FirstName"));
+		Log.info("Entering  the FirstName into the FirstName Textfield");
+		assertEquals(checkout1.FirstName.getAttributeValue("value"), data.get("Assertion2"),
+				"FirstName is not entered into the FirstName Textfield");
+		checkout1.LastName.enterText(data.get("LastName"));
+		Log.info("Entering  the LastName into the LastName Textfield");
+		assertEquals(checkout1.LastName.getAttributeValue("value"), data.get("Assertion3"),
+				"LastName is not entered into the LastName Textfield");
+		checkout1.Email.enterText(data.get("Email"));
+		Log.info("Entering  the Email into the Email Textfield");
+		assertEquals(checkout1.Email.getAttributeValue("value"), data.get("Assertion4"),
+				"Email is not entered into the LastName Textfield");
+		checkout1.CountryIdInBilling.selectByVisibleText(data.get("Country Dropdown Text"));
+		assertEquals(checkout1.CountryIdInBilling.getSelectedOption(), data.get("Assertion5"),
+				"Incorrect option selected from dropdown");
+		checkout1.City.enterText(data.get("City"));
+		assertEquals(checkout1.City.getAttributeValue("value"), data.get("Assertion6"), "Incorrect City");
+		checkout1.Address1InBilling.enterText(data.get("Address1"));
+		assertEquals(checkout1.Address1InBilling.getAttributeValue("value"), data.get("Assertion7"),
+				"Incorrect Address1");
+		checkout1.ZipPostalCodeInBilling.enterText(data.get("Zip/Postal Code"));
+		assertEquals(checkout1.ZipPostalCodeInBilling.getAttributeValue("value"), data.get("Assertion8"),
+				"Incorrect Zip/Postal Code");
+		checkout1.PhoneNumberInBilling.enterText(data.get("Phone Number"));
+		assertEquals(checkout1.PhoneNumberInBilling.getAttributeValue("value"), data.get("Assertion9"),
+				"Incorrect Phone Number");
+		assertTrue(checkout1.ContinueInBilling.isEnabled(), "Continue In Billing is  disabled");
+		checkout1.ContinueInBilling.click();
+
+		// ShippingAddressPage
+		assertTrue(checkout1.ShippingAddressFeild.isVisible(), "Shipping Address Feild is not displayed");
+		checkout1.InStorePickup.check();
+		assertTrue(checkout1.InStorePickup.isChecked(), "InStorePickup  Checkbox is not checked");
+		checkout1.ContinueinShipping.click();
+
+		// Payment Method
+		assertTrue(checkout1.PaymentMethodField.isVisible(), "Payment Method Feild is not displayed");
+		checkout1.CheckMoneyOrder.click();
+		assertTrue(checkout1.ContinueInPaymentMethod.isEnabled(), "Continue In Payment Method is  disabled");
+		checkout1.ContinueInPaymentMethod.click();
+
+		// Payment Information
+		assertTrue(checkout1.PaymentInformationField.isVisible(), "Payment Information Feild is not displayed");
+		assertTrue(checkout1.ContinueInPaymentInformation.isEnabled(), "Continue In Payment Information  is  disabled");
+		checkout1.ContinueInPaymentInformation.click();
+
+		// Confirmation Order
+		assertTrue(checkout1.ConfirmOrderField.isVisible(), "Confirm Order Feild is not displayed");
+		assertTrue(checkout1.BillingAddressText.isVisible(), "Billing  Address Text  is not displayed");
+		assertTrue(checkout1.ConfirmInConfirmationOrder.isEnabled(), "Confirm In ConfirmationOrder is  diasbled");
+		checkout1.ConfirmInConfirmationOrder.click();
+
+		// ThankYou Page
+		assertTrue(checkout1.ThankYouPage.isVisible(), "ThankYouPage is not displayed");
+		assertTrue(checkout1.OrderNumber.isVisible(), "OrderNumber is not displayed");
+		assertTrue(checkout1.ConfirmationMessage.isVisible(), "ConfirmationMessage is not displayed");
+		assertTrue(checkout1.ContinueInThankYouPage.isEnabled(), "Continue In ThankYouPage is  disabled");
+		checkout1.ContinueInThankYouPage.click();
+		assertTrue(checkout1.ApplicationHomePage.isVisible(), "Application HomePage is displayed");
 
 	}
 
-
-
+}

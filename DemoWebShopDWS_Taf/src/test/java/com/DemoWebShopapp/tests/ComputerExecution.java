421900da8f0b7498c9ce44e2829c2b2ae5794426
Preeti <Preeti@DESKTOP-85FK620>
2022-11-15 13:06:16 +0530

    Updated Test Script on ComputerExecution.java and Excel Data

--- DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/ComputerExecution.java
+++ DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/ComputerExecution.java
@@ -15,19 +15,16 @@ public class ComputerExecution extends BaseTest {
 	@Test(dataProvider = "ExcelDataProvider")
 
 	public void checkout(Map<String, String> data) throws InterruptedException {
-//		HomePageDWS home = new HomePageDWS();
-//		LoginPageDWS login = home.login.click(LoginPageDWS.class);
-//		login.email.enterText(Configuration.get("username"));
-//		login.password.enterText(Configuration.get("password"));
-//		login.loginButton.click(LoginPageDWS.class);
 
 		// LOGIN PAGE
 		HomePageDWS home = new HomePageDWS();
 		LoginPageDWS login = home.login.click(LoginPageDWS.class);
 		login.email.enterText(data.get("Username"));
-		assertEquals(login.email.getAttributeValue("value"), data.get("Assertion1"),
+		assertEquals(login.email.getAttributeValue("value"), data.get("Username_Assertion"),
 				"Email is not entered into Email textfield");
 		login.password.enterText(data.get("Password"));
+		assertEquals(login.password.getAttributeValue("value"), data.get("Password_Assertion"),
+				"password is not entered into password textfield");
 		login = login.loginButton.click(LoginPageDWS.class);
 
 		// DESKTOPS PAGE
@@ -56,41 +53,41 @@ public class ComputerExecution extends BaseTest {
 		// CHECKOUT PAGE
 		CheckoutPage checkout1 = new CheckoutPage();
 		assertTrue(checkout1.CheckoutPage.isVisible(), "Checkoutpage is not displayed");
-		
+
 		// BILLING ADDRESS
 		checkout1.BillingAddressDropDown.selectByVisibleText(data.get("Dropdown Visible Text"));
-		assertEquals(checkout1.BillingAddressDropDown.getSelectedOption(), data.get("Assertion3"),
+		assertEquals(checkout1.BillingAddressDropDown.getSelectedOption(), data.get("Assertion1"),
 				"Incorrect option selected from dropdown");
 
 		checkout1.FirstName.enterText(data.get("FirstName"));
-		assertEquals(checkout1.FirstName.getAttributeValue("value"), data.get("Assertion4"),
+		assertEquals(checkout1.FirstName.getAttributeValue("value"), data.get("Assertion2"),
 				"FirstName is not entered into FirstName textfield");
 
 		checkout1.LastName.enterText(data.get("LastName"));
-		assertEquals(checkout1.LastName.getAttributeValue("value"), data.get("Assertion5"),
+		assertEquals(checkout1.LastName.getAttributeValue("value"), data.get("Assertion3"),
 				"LastName is not entered into LastName textfield");
 
 		checkout1.Email.enterText(data.get("Email"));
-		assertEquals(checkout1.Email.getAttributeValue("value"), data.get("Assertion6"),
+		assertEquals(checkout1.Email.getAttributeValue("value"), data.get("Assertion4"),
 				"Email is not entered into Email textfield");
 
 		checkout1.CountryIdInBilling.selectByVisibleText(data.get("Country Dropdown Text"));
-		assertEquals(checkout1.CountryIdInBilling.getSelectedOption(), data.get("Assertion7"),
+		assertEquals(checkout1.CountryIdInBilling.getSelectedOption(), data.get("Assertion5"),
 				"Incorrect option selected from dropdown");
 
 		checkout1.City.enterText(data.get("City"));
-		assertEquals(checkout1.City.getAttributeValue("value"), data.get("Assertion8"), "Incorrect City");
+		assertEquals(checkout1.City.getAttributeValue("value"), data.get("Assertion6"), "Incorrect City");
 
 		checkout1.Address1InBilling.enterText(data.get("Address1"));
-		assertEquals(checkout1.Address1InBilling.getAttributeValue("value"), data.get("Assertion9"),
+		assertEquals(checkout1.Address1InBilling.getAttributeValue("value"), data.get("Assertion7"),
 				"Incorrect Address");
 
 		checkout1.ZipPostalCodeInBilling.enterText(data.get("Zip/Postal Code"));
-		assertEquals(checkout1.ZipPostalCodeInBilling.getAttributeValue("value"), data.get("Assertion10"),
+		assertEquals(checkout1.ZipPostalCodeInBilling.getAttributeValue("value"), data.get("Assertion8"),
 				"Incorrect Zip/Postal Code");
 
 		checkout1.PhoneNumberInBilling.enterText(data.get("Phone Number"));
-		assertEquals(checkout1.PhoneNumberInBilling.getAttributeValue("value"), data.get("Assertion11"),
+		assertEquals(checkout1.PhoneNumberInBilling.getAttributeValue("value"), data.get("Assertion9"),
 				"Incorrect phone number");
 		checkout1.ContinueInBilling.click();
 

483dab90677318e83ce7936eb2a9f1bf5e1b4c2f
Anilkumarsm28 <anilkumarsm28@gmail.com>
2022-11-15 10:36:54 +0530

    Updated ElectronicsExecution.java & ElectronicsPage.java

--- DemoWebShopDWS_Taf/src/main/java/com/DemoWebShopapp/pages/ElectronicsPage.java
+++ DemoWebShopDWS_Taf/src/main/java/com/DemoWebShopapp/pages/ElectronicsPage.java
@@ -7,12 +7,18 @@ import org.automation.elements.DropDown;
 import org.automation.elements.HyperLink;
 import org.automation.elements.TextBox;
 import org.openqa.selenium.By;
-
+/***
+ * 
+ * @author TYS-AnilKumar SM
+ * @version 1.0.0
+ * @since 14/11/2022
+ *
+ */
 public class ElectronicsPage extends BasePage {
 
 	public final HyperLink Electronics = new HyperLink("Electronics button",
 			By.xpath("(//a[contains(text(),'Cell phones')])[4]"));
-	
+	public final org.automation.elements.Element electronicsPage=new org.automation.elements.Element("electronicspage", By.xpath("//h1[text()='Electronics']"));
 	public final DropDown SortBy = new DropDown("SortBy DropDown", By.id("products-orderby"));
 	public final DropDown SortbyPosition=new DropDown("SortbyPosition DropDown",By.xpath("//option[text()='Position']"));
 	public final DropDown SortbyAtoZ=new DropDown("SortbyAtoZ DropDown",By.xpath("//option[text()='Name: A to Z']"));
@@ -27,12 +33,13 @@ public class ElectronicsPage extends BasePage {
 	public final DropDown Display12PerPage = new DropDown("Display12PerPage DropDown", By.xpath("//option[text()='12']"));
 
 	
+	
 	public final DropDown viewAs = new DropDown("ViewAs DropDown", By.id("products-viewmode"));
 	public final DropDown viewAsGrid = new DropDown("viewAsGrid DropDown", By.xpath("//option[text()='Grid']"));
 	public final DropDown viewAsList = new DropDown("viewAsList DropDown", By.xpath("//option[text()='List']"));
     
-	public final HyperLink cellphones=new HyperLink("cellphones link", By.xpath("//a[@href='/cell-phones'][@title='Show products in category Cell phones'][contains(text(),'Cell phones')]"));
-	public final HyperLink Smartphone = new HyperLink("Smartphone link", By.xpath("//a[text()='Smartphone']"));
+	public final HyperLink cellphones=new HyperLink("cellphones link", By.xpath("//h2[@class='title']//a[contains(text(),'Cell phones')]"));
+	public final HyperLink Smartphone = new HyperLink("Smartphone link", By.xpath("//h2[@class='product-title']//a[text()='Smartphone']"));
 	public final TextBox Qty = new TextBox("Qty Textbox", By.id("addtocart_43_EnteredQuantity"));
 	public final Button AddToCart = new Button("AddToCart Button",
 			By.xpath("//input[@class='button-1 add-to-cart-button']"));
@@ -46,6 +53,6 @@ public class ElectronicsPage extends BasePage {
 	public final DropDown PostalCode = new DropDown("PostalCode DropDown", By.xpath("//input[@id='ZipPostalCode']"));
 	public final CheckBox TermsAndService = new CheckBox("TermsAndService DropDown",
 			By.xpath("//input[@id='termsofservice']"));
-	public final Button CheckOut = new Button("CheckOut Button", By.xpath("//button['checkout']"));
+	public final Button CheckOutbutton = new Button("CheckOut Button", By.xpath("//button['checkout']"));
    
 }
--- DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/ElectronicsExecution.java
+++ DemoWebShopDWS_Taf/src/test/java/com/DemoWebShopapp/tests/ElectronicsExecution.java
@@ -1,48 +1,123 @@
 package com.DemoWebShopapp.tests;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-
-import org.automation.base.BasePage;
+import java.util.Map;
 import org.automation.base.BaseTest;
-import org.automation.utilities.Configuration;
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-
+import com.DemoWebShopapp.pages.CheckoutPage;
 import com.DemoWebShopapp.pages.ElectronicsPage;
 import com.DemoWebShopapp.pages.HomePageDWS;
 import com.DemoWebShopapp.pages.LoginPageDWS;
 
-public class ElectronicsExecution extends BaseTest  {
-    @Test
-	public void electronics() { 
-	HomePageDWS home = new HomePageDWS();
-	
-	LoginPageDWS login=home.login.click(LoginPageDWS.class);
-	login.email.enterText(org.automation.utilities.Configuration.get("username"));;
-	login.password.enterText(Configuration.get("password"));
-	login.loginButton.click(LoginPageDWS.class);
-	
-	ElectronicsPage electronics=home.electronics.click(ElectronicsPage.class);
-	electronics.cellphones.click(ElectronicsPage.class);
-	assertTrue(electronics.SortbyPosition.isVisible(), "sortByPosition DropDown is not visible");
-	assertTrue(electronics.Display8PerPage.isVisible(), "Display8PerPage DropDown is not visible");
-	assertTrue(electronics.viewAsGrid.isVisible(), "viewAsGrid Dropdown is not visible");
-	assertTrue(electronics.Smartphone.isVisible(),"Smaertphone link is not visible");
-	electronics.Smartphone.click(ElectronicsPage.class);
-	assertTrue(electronics.Qty.isVisible(), "Qty Textfield is not visible");
-	assertTrue(electronics.AddToCart.isVisible(), "AddToCart button is not visible");
-	electronics.AddToCart.click(ElectronicsPage.class);
-	assertTrue(electronics.confmsg.isVisible(), "conf msg is not visible");
-	electronics.ShoppingCart.click(ElectronicsPage.class);
-	assertTrue(electronics.CountryId.isVisible(), "CountryId is not visible");
-	assertTrue(electronics.StateOrProvince.isVisible(), "StateOrProvince is not visible");
-	assertTrue(electronics.PostalCode.isVisible(), "ZipOrPostal code is not visible");
-	assertTrue(electronics.TermsAndService.isVisible(), "Terms and Conditions checkbox is not visible");
-	assertTrue(electronics.CheckOut.isVisible(), "Checkout button is not visible");
-	electronics.CountryId.getWebElement().findElements(By.xpath("//option[text()='India']"));
-	
-	
+/***
+ * 
+ * @author TYS-AnilKumar SM
+ * @version 1.0.0
+ * @since 14/11/2022
+ *
+ */
 
+public class ElectronicsExecution extends BaseTest {
 
-}
+	@Test(dataProvider = "ExcelDataProvider")
+	public void checkout(Map<String, String> data) throws InterruptedException {
+		// Home Page.
+		HomePageDWS home = new HomePageDWS();
+
+		// Login Page.
+		LoginPageDWS login = home.login.click(LoginPageDWS.class);
+		login.email.enterText(data.get("Username"));
+		assertEquals(login.email.getAttributeValue("value"), data.get("Assertion1"),
+				"Email is not entered in Email Textfeild");
+		login.password.enterText(data.get("Password"));
+		assertEquals(login.password.getAttributeValue("value"), data.get("Assertion2"),
+				"password is not entered into password textfield");
+		login.loginButton.click(LoginPageDWS.class);
+
+		// Electronics Page.
+		ElectronicsPage electronics = home.electronics.click(ElectronicsPage.class);
+		electronics.cellphones.click(ElectronicsPage.class);
+		assertTrue(electronics.SortbyPosition.isVisible(), "sortByPosition DropDown is not visible");
+		assertTrue(electronics.Display8PerPage.isVisible(), "Display8PerPage DropDown is not visible");
+		assertTrue(electronics.viewAsGrid.isVisible(), "viewAsGrid Dropdown is not visible");
+		assertTrue(electronics.Smartphone.isVisible(), "Smartphone link is not visible");
+
+		electronics.Smartphone.click(ElectronicsPage.class);
+		assertTrue(electronics.Qty.isVisible(), "Qty Textfield is not visible");
+		assertTrue(electronics.AddToCart.isVisible(), "AddToCart button is not visible");
+
+		electronics.AddToCart.click(ElectronicsPage.class);
+		assertTrue(electronics.confmsg.isVisible(), "conf msg is not visible");
+
+		electronics.ShoppingCart.click(ElectronicsPage.class);
+		electronics.TermsAndService.check();
+		assertTrue(electronics.TermsAndService.isChecked(), "Terms and service checkbox is not checked");
+
+		electronics.CheckOutbutton.click();
+		CheckoutPage checkout = new CheckoutPage();
+		assertTrue(checkout.BillingAddressField.isVisible(), "Checkout button is not Clicking");
+
+		// Checkout page.
+		CheckoutPage checkout1 = new CheckoutPage();
+		assertTrue(checkout1.CheckoutPage.isVisible(), "Checkoutpage is not  displayed");
+        
+		//Billing Address.
+		checkout1.BillingAddressDropDown.selectByVisibleText(data.get("Dropdown Visible Text"));
+		checkout.FirstName.enterText(data.get("FirstName"));
+		assertEquals(checkout.FirstName.getAttributeValue("value"), data.get("Assertion3"), "Incorrect First Name");
+
+		checkout.LastName.enterText(data.get("LastName"));
+		assertEquals(checkout.LastName.getAttributeValue("value"), data.get("Assertion4"), "Incorrect First Name");
+
+		checkout.Email.enterText(data.get("Email"));
+		assertEquals(checkout.Email.getAttributeValue("value"), data.get("Assertion5"), "Incorrect Email");
+
+		checkout.CountryIdInBilling.selectByVisibleText(data.get("Country Dropdown Text"));
+		assertEquals(checkout.CountryIdInBilling.getSelectedOption(), data.get("Assertion10"),
+				"Incorrect option selected from dropdown");
+
+		checkout1.City.enterText(data.get("City"));
+		assertEquals(checkout1.City.getAttributeValue("value"), data.get("Assertion6"), "Incorrect City");
+
+		checkout1.Address1InBilling.enterText(data.get("Address1"));
+		assertEquals(checkout1.Address1InBilling.getAttributeValue("value"), data.get("Assertion7"),
+				"Incorrect Address");
+
+		checkout1.ZipPostalCodeInBilling.enterText(data.get("Zip/Postal Code"));
+		assertEquals(checkout1.ZipPostalCodeInBilling.getAttributeValue("value"), data.get("Assertion8"),
+				"Incorrect Zip/Postal Code");
+
+		checkout1.PhoneNumberInBilling.enterText(data.get("Phone Number"));
+		assertEquals(checkout1.PhoneNumberInBilling.getAttributeValue("value"), data.get("Assertion9"),
+				"Incorrect phone number");
+		checkout1.ContinueInBilling.click();
+
+		//Shipping Address.
+		assertTrue(checkout1.ShippingAddressFeild.isVisible(), "Shipping Address Feild is not displayed");
+		checkout1.InStorePickup.check();
+		assertTrue(checkout1.InStorePickup.isChecked(), "InStorePickup  Checkbox is not checked");
+		checkout1.ContinueinShipping.click();
+
+		//Payment Method.
+		assertTrue(checkout1.PaymentMethodField.isVisible(), "PaymentMethodField page is not displayed");
+		checkout1.CashOnDelivery.click();
+		checkout1.ContinueInPaymentMethod.click();
+		
+		//Payment Information.
+		assertTrue(checkout1.PaymentInformationField.isVisible(), "Payment information page is displayed");
+		assertTrue(checkout1.CodText.isVisible(), "You will pay by cash on delivery message is not displayed");
+		checkout1.ContinueInPaymentInformation.click();
+		assertTrue(checkout1.ConfirmOrderField.isVisible(), "Confirm Order Feild is not displayed");
+		assertTrue(checkout1.BillingAddressText.isVisible(), "Billing  Address Text  is not displayed");
+		
+		//Confirmation page.
+		checkout1.ConfirmInConfirmationOrder.click();		
+		assertTrue(checkout1.ThankYouPage.isVisible(), "Thank you page is not displayed");
+		assertTrue(checkout1.OrderNumber.isVisible(), "Order number 1315200 is not displayed");
+		
+		//Thank you page.
+		checkout1.ContinueInThankYouPage.click();
+		assertTrue(checkout1.ApplicationHomePage.isVisible(), "Demo web shop home page is not displayed");
+	}
 }
